ee2ce80da47f401b8fafcb43941f98617ccbad11
Herve <rvonthecode@gmail.com>
2020-08-19 16:32:24 -0300

    ..

--- challenge_two/university.php
+++ challenge_two/university.php
@@ -1,26 +1,13 @@
 <?php 
 	
-
-// Ideally the connection is handled elsewhere in the code. Using it here for brevity sake. 	
-try {
-    $pdo = new PDO("mysql:host=127.0.0.1;dbname=nordech_challenge", "root", "Gherve2016", [
-                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                        PDO::ATTR_EMULATE_PREPARES => false,
-                        ]
-                    );
-} catch(\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
-};
 	
-	
-	class University
+class University
 {
     protected $connection = null;
 
     public function __construct()
     {
-        $this->connection = $pdo;
+        $this->connection = new PDO("mysql:host=127.0.0.1;dbname=nordech_challenge", "root", "Gherve2016");
     }
     
     public function addUniversity($country, $name, $domain){
@@ -31,13 +18,15 @@ try {
     $stmt->execute([$setValue, $searchValue]);
     return $stmt->rowCount();	    
     }
-
+ 
     public function getUniversity()
     {
         $sql = 'SELECT * FROM universities';
         $result = $this->connection->query($sql);
         $row = $result->fetch(PDO::FETCH_ASSOC);
-
+        
+        $resultArray = array();
+       
         foreach($row as $column => $value) {
             $this->$column = $value;
         }
