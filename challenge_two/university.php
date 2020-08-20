4a336f1f96e62c25f69d1152013b44edb15803af
Herve <rvonthecode@gmail.com>
2020-08-20 16:53:25 -0300

    Update DB connexion with pdo.

--- challenge_two/university.php
+++ challenge_two/university.php
@@ -1,35 +1,60 @@
 <?php 
 	
 	
+	    $host = '127.0.0.1';
+		$db   = 'nordech_challenge';
+		$user = 'root';
+		$pass = 'Gherve2016';
+		$port = "3306";
+		
+		$connexionString = "mysql:host=$host;dbname=$db;port=$port";
+		
+		$options = [
+		\PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+		\PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+		\PDO::ATTR_EMULATE_PREPARES   => false,
+		];
+	
 class University
 {
-    protected $connection = null;
-
+	   
     public function __construct()
     {
-        $this->connection = new PDO("mysql:host=127.0.0.1;dbname=nordech_challenge", "root", "Gherve2016");
+		 global $host, $db, $user, $pass, $port, $connexionString, $options;
+		 	
+        $this->$pdo = new \PDO($connexionString, $user, $pass, $options);
+        
     }
     
     public function addUniversity($country, $name, $domain){
-    $query = "INSERT INTO `universities` (`uni_country`, `uni_name`, `uni_domain`)"
-         ."VALUES ($country, $name, $domain)";
-         
-    $stmt = $pdo->prepare($query);
-    $stmt->execute([$setValue, $searchValue]);
-    return $stmt->rowCount();	    
-    }
- 
-    public function getUniversity()
-    {
-        $sql = 'SELECT * FROM universities';
-        $result = $this->connection->query($sql);
-        $row = $result->fetch(PDO::FETCH_ASSOC);
+	    
+           $country = addslashes($country);
+           $name = addslashes($name);
+           $domain =addslashes($domain);
+	    
+	    	   
+	    $sql = "INSERT INTO universities (`uni_country`, `uni_name`, `uni_domain`)"
+	         ." VALUES(:country, :name, :domain)";
+	         
+	    $stmt = $this->$pdo->prepare($sql)->execute([$country, $name, $domain]);     
+
         
-        $resultArray = array();
-       
-        foreach($row as $column => $value) {
-            $this->$column = $value;
-        }
-    }
+    }    
+    
 }
+
+
+    function listOfUniversities()
+    {
+	    global $host, $db, $user, $pass, $port, $connexionString, $options;
+	    $pdo = new \PDO($connexionString, $user, $pass, $options);
+	    
+	    
+        $sql = 'SELECT * FROM universities';
+        $resultArray = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+                
+        return $resultArray;        
+        
+    }
+
 ?>
